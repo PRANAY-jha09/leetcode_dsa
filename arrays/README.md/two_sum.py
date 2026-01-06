c7eab4eaf142d124d3c60b7f79f9c15fda469a47
PRANAY JHA <pranayjha535@gmail.com>
2026-01-06 10:59:07 +0530

    Add Two Sum function to README
    Added implementation for the Two Sum problem from Leetcode.

---
+++ arrays/README.md/two_sum.py
@@ -0,0 +1,11 @@
+# Leetcode : Two sum 
+def twoSum (num,target):
+  hashmap ={}
+  for i , num in 
+enumerate (nums):
+  diff =target-num 
+  if diff in hashmap:
+    return 
+[hashmap[diff],i] 
+        hashmap[num] =i
+print(twoSum ([2,7,11,15],9))
